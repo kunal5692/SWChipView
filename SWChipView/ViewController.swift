f693713ced78cea7f8de869b1c768b933e1e2440
Sudam Chavan <kunal@sudams-macbook-pro.local>
2016-06-14 10:42:52 +0530

    Initial Commit

---
+++ SWChipView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SWChipView
+//
+//  Created by Sudam Chavan on 6/14/16.
+//  Copyright © 2016 kunal chavhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
